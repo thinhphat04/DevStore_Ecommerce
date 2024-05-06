a3559c7817198fd4782182043ac359227767976b
ngothinhphat <ngothinhphat244@gmail.com>
2024-05-06 23:34:32 +0700

    update

--- FrontEnd/client/src/features/home/HomePage.tsx
+++ FrontEnd/client/src/features/home/HomePage.tsx
@@ -16,29 +16,35 @@ export default function HomePage() {
           <img
             src="/images/homepage1.jpg"
             alt="hero"
-            style={{ display: "block", width: "100%", maxHeight: 500 }}
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
           />
         </div>
         <div>
           <img
             src="/images/homepage2.jpg"
             alt="hero"
-            style={{ display: "block", width: "100%", maxHeight: 500 }}
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
           />
         </div>
         <div>
           <img
             src="/images/homepage3.jpg"
             alt="hero"
-            style={{ display: "block", width: "100%", maxHeight: 500 }}
+            style={{ display: "block", width: "100%", maxHeight: 600 }}
           />
         </div>
       </Slider>
       <Box display='flex' justifyContent='center' sx={{ p: 4 }}>
         <Typography variant='h3'>
-          Welcome to the DEV-Store
+          Welcome to the DEV-Store E-commerce Website
         </Typography>
       </Box>
+      <Box display='flex' justifyContent='center' sx={{ p: 4 }}>
+        <Typography variant='h4'>
+          SourceCode: <a href="https://github.com/thinhphat04/DevStore_Ecommerce">https://github.com/thinhphat04/DevStore_Ecommerce</a>
+        </Typography>
+      </Box>
+
     </>
   );
 }
